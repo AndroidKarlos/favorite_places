118394abb0066cc44ff7353cf98740dbf03e8f6f
José Carlos B <carlos.bezerradasilva@hotmail.com>
2023-10-28 16:19:26 -0300

    add image in places_list, provider and place_detail

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,13 +1,12 @@
-
-
+import 'dart:io';
 import 'package:favorite_places/model/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlaces(String title){
-    final newPlace = Place(title: title);
+  void addPlaces(String title, File image){
+    final newPlace = Place(title: title, image: image);
     state = [newPlace, ...state];
   }
 }
--- lib/screen/add_place.dart
+++ lib/screen/add_place.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/widget/image_input.dart';
 import 'package:flutter/material.dart';
@@ -14,14 +15,15 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace(){
     final enteredTitle = _titleController.text;
 
-    if(enteredTitle.isEmpty){
+    if(enteredTitle.isEmpty || _selectedImage == null){
       return;
     }
-    ref.read(userPlacesProvider.notifier).addPlaces(enteredTitle);
+    ref.read(userPlacesProvider.notifier).addPlaces(enteredTitle, _selectedImage!);
     Navigator.of(context).pop();
   }
 
@@ -47,7 +49,9 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
             ),
             const SizedBox(height: 10),
-            const ImageInput(),
+            ImageInput(onPickImage:(image) {
+              _selectedImage = image;
+            },),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
